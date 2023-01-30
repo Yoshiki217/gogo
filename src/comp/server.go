3ff793c99e888d4fe27e8f79eab2c47f4f6cb6ca
yoshiki <effort0217@icloud.com>
2023-01-30 21:57:00 +0900

    add/middleware

--- src/comp/server.go
+++ src/comp/server.go
@@ -4,16 +4,21 @@ import (
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
+
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello!")
+}
 
 func main() {
 	e := echo.New()
 
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
 	e.GET("/", hello)
 
 	e.Logger.Fatal(e.Start(":1323"))
-}
-
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello!")
 }
